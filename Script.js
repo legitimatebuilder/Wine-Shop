eec22449010bc3d1a26355b6077c7c7510a6e6c7
Waqas Ali <aliwaqas701@yahoo.com>
2026-09-24 15:20:43 +0500

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,3 @@
+gsap.from('.intro-text-container', { duration: 1.3, y: '-200%', ease: 'power1.in' });
+gsap.from('.intro-image-container', { duration: 1.3, y: '-200%', ease: 'power2.in' });
+gsap.from('.nav-bar', { duration: 1, y: '-100%', ease: 'bounce', delay: 1.5 });
